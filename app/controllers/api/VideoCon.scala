a2adf37251e40984c15effc210100266da19390a
exca <excamuchlisandita@gmail.com>
2023-12-17 10:47:19 +0700

    upload video

--- app/controllers/api/VideoCon.scala
+++ app/controllers/api/VideoCon.scala
@@ -5,12 +5,16 @@ import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 import utils.{Helpers, ListResult, ResponseService => Res}
 import play.api.libs.Files.TemporaryFile
+import play.api.Configuration 
 
 import javax.inject.{Inject, Singleton}
 import java.nio.file.{Files, Path, Paths}
+import play.api.libs.Files.TemporaryFile
+import play.api.mvc.MultipartFormData
 
 @Singleton
 class VideoCon @Inject()(
+                              config: Configuration,
                               cc: ControllerComponents,
                               videoData: VideoData
                               ) extends AbstractController(cc){
@@ -36,44 +40,110 @@ class VideoCon @Inject()(
     }
   }
 
-  def add(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    val param = request.body.asJson.get
-    val name = (param \ "name").as[String]
-    val path = (param \ "path").as[String]
-    val status = (param \ "status").asOpt[Int].getOrElse(1)
+//   def add(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+//     val param = request.body.asJson.get
+//     val name = (param \ "name").as[String]
+//     val path = (param \ "path").as[String]
+//     val status = (param \ "status").asOpt[Int].getOrElse(1)
 
-    val video = Video(-1, name, path, status)
+//     val video = Video(-1, name, path, status)
 
-    val res = videoData.insert(video)
-    res match {
-      case (Some(data), _) =>
-        Res.created(
-          "Data Video berhasil ditambahkan.",
-          Json.obj("video_id" -> data.asInstanceOf[Long]))
+//     val res = videoData.insert(video)
+//     res match {
+//       case (Some(data), _) =>
+//         Res.created(
+//           "Data Video berhasil ditambahkan.",
+//           Json.obj("video_id" -> data.asInstanceOf[Long]))
 
-      case (None, message) => {
-          Res.badRequest(s"Data Video gagal ditambahkan.", message)
-        }
+//       case (None, message) => {
+//           Res.badRequest(s"Data Video gagal ditambahkan.", message)
+//         }
+//     }
+//   }
+
+  def add(): Action[MultipartFormData[TemporaryFile]] = Action(parse.multipartFormData) { request =>  
+    request.body.file("video").map { videoFile =>
+      val name = request.body.dataParts("name").head
+      val status = request.body.dataParts("status").headOption.map(_.toInt).getOrElse(1)
+
+      val filename = videoFile.filename
+      val contentType = videoFile.contentType
+      val videoDir: String = config.get[String]("app.videoDir")
+      val destinationPath: Path = Paths.get(s"$videoDir", filename)
+
+      // Simpan file video ke folder yang diinginkan
+      videoFile.ref.moveFileTo(destinationPath.toFile)
+
+      // Selanjutnya, lakukan operasi penyimpanan data ke database
+      val video = Video(-1, name, filename, status)
+      val res = videoData.insert(video)
+
+      res match {
+        case (Some(data), _) =>
+          Ok(
+            Json.obj("message" -> "Data Video berhasil ditambahkan.", "video_id" -> data.asInstanceOf[Long])
+          )
+
+        case (None, message) =>
+          BadRequest(
+            Json.obj("message" -> s"Data Video gagal ditambahkan.", "error" -> message)
+          )
+      }
+    }.getOrElse {
+      BadRequest(Json.obj("message" -> "File video tidak ditemukan"))
     }
   }
 
-  def put(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  def put(): Action[MultipartFormData[TemporaryFile]] = Action(parse.multipartFormData) { request =>  
+    request.body.file("video").map { videoFile =>
+      val id = request.body.dataParts("id").head.toInt
+      val name = request.body.dataParts("name").head
+      val status = request.body.dataParts("status").headOption.map(_.toInt).getOrElse(1)
 
-    val param = request.body.asJson.get
-    val video = param.as[Video]
+      val filename = videoFile.filename
+      val contentType = videoFile.contentType
+      val videoDir: String = config.get[String]("app.videoDir")
+      val destinationPath: Path = Paths.get(s"$videoDir", filename)
 
-    val res = videoData.update(video)
-    res match {
-      case (1, "") =>
-        Res.success[JsValue]("Data Video berhasil diperbaharui.",
-          Json.obj(
-            "video_id" -> video.id,
-          ))
-      case (-1, message) => {
-          Res.badRequest(s"Data Video perbaharui.", message)
+      // Simpan file video ke folder yang diinginkan
+      videoFile.ref.moveFileTo(destinationPath.toFile)
+
+      // Selanjutnya, lakukan operasi penyimpanan data ke database
+      val video = Video(id, name, filename, status)
+      val res = videoData.update(video)
+
+      res match {
+        case (1, "") =>
+            Res.success[JsValue]("Data Video berhasil diperbaharui.",
+            Json.obj(
+                "video_id" -> video.id,
+            ))
+        case (-1, message) => {
+            Res.badRequest(s"Data Video perbaharui.", message)
+            }
         }
+    }.getOrElse {
+      BadRequest(Json.obj("message" -> "File video tidak ditemukan"))
     }
   }
+
+//   def put(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+
+//     val param = request.body.asJson.get
+//     val video = param.as[Video]
+
+//     val res = videoData.update(video)
+//     res match {
+//       case (1, "") =>
+//         Res.success[JsValue]("Data Video berhasil diperbaharui.",
+//           Json.obj(
+//             "video_id" -> video.id,
+//           ))
+//       case (-1, message) => {
+//           Res.badRequest(s"Data Video perbaharui.", message)
+//         }
+//     }
+//   }
 
   def remove(): Action[AnyContent] = Action { implicit request =>
     val param = request.body.asJson.get
